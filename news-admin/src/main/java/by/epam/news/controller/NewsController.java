3d058204e1f2be5a662cfad28b5c65913844d153
Maryia Tsiaseika <maryia_tsiaseika@epam.com>
2016-06-23 13:03:20 +0300

    Add news list view

--- news-admin/src/main/java/by/epam/news/controller/NewsController.java
+++ news-admin/src/main/java/by/epam/news/controller/NewsController.java
@@ -1,24 +1,47 @@
 package by.epam.news.controller;
 
+import by.epam.news.domain.Author;
 import by.epam.news.domain.NewsDTO;
+import by.epam.news.domain.Tag;
+import by.epam.news.service.AuthorService;
 import by.epam.news.service.NewsService;
 import by.epam.news.service.ServiceException;
+import by.epam.news.service.TagService;
+import org.hibernate.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping(value="/news")
 public class NewsController {
     @Autowired
     private NewsService newsService;
+    @Autowired
+    private AuthorService authorService;
+    @Autowired
+    private TagService tagService;
+    private static final Long newsLimitPerPage = 3L;
 
-    @RequestMapping(value="/view/message/{newsId}")
+    @RequestMapping(value="/message/{newsId}")
     public String viewMessage(Model model, @PathVariable("newsId") Long newsId) throws ServiceException {
         NewsDTO newsDTO = newsService.findById(newsId);
         model.addAttribute("newsDTO", newsDTO);
         return "newsMessage";
+    }
+
+    @RequestMapping(value = "/list/{page}")
+    public String viewList(Model model, @PathVariable("page") Long page) throws ServiceException {
+        List<NewsDTO> newsDTOList = newsService.findAllNews(page, newsLimitPerPage);
+        List<Author> authors = authorService.findNotExpiredAuthors();
+        List<Tag> tags = tagService.findAll();
+        model.addAttribute("newsDTOList", newsDTOList);
+        model.addAttribute("authors", authors);
+        model.addAttribute("tags", tags);
+        return "newsList";
     }
 }
